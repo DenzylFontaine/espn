bf347c50fa6118e5c3c188619f645ed537bbf442
DenzylFontaine <ddenzfong@gmail.com>
2016-01-17 21:49:18 -0700

    added diagram

--- index.php
+++ index.php
@@ -6,5 +6,18 @@
 	<body>
 		<h1>ESPN Entity Relationship</h1>
 		<img src="Entity Relationship Diagram.svg" />
+		<h2>Persona</h2>
+		<P>The crowd the NBA would like to attract is men of all ages who wish to see basketball played at its highest level.<br/>
+		Also any males who is a fan of one of the NBA's many stars.<br/>
+		The NBA would also like to have a large portion of female viewers as well.<br/>
+		Mostly teenage to middle age women who have played before or simply fans of the game.<br/>
+		Much like the men the women would also have favorite players and stars who thwy would tune in to watch.
+		</P>
+	<div>
+		<h3>Use Case</h3>
+		<p>
+
+		</p>
+	</div>
 	</body>
 </html>
