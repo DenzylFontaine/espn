578f8650f7b835e9ab862d590ab2a679f2bdcacf
DenzylFontaine <ddenzfong@gmail.com>
2016-01-20 10:35:30 -0700

    added 2nd picture

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
 	</head>
 	<body>
 		<h1>ESPN Entity Relationship</h1>
-		<img src="Entity Relationship Diagram.svg" />
+		<img src="Entity Relationship Diagram.svg" >
+		<img class="pic" src="http://photon.101medialablimit.netdna-cdn.com/hypebeast.com/image/2015/12/kobe-bryant-greatest-players-list-1.jpg?w=1024"/>
 		<h2>Persona</h2>
 		<P>The crowd the NBA would like to attract is men of all ages who wish to see basketball played at its highest level.<br/>
 		Also any males who is a fan of one of the NBA's many stars.<br/>
@@ -15,7 +16,6 @@
 		Mostly teenage to middle age women who have played before or simply fans of the game.<br/>
 		Much like the men the women would also have favorite players and stars who they would tune in to watch.
 		</P>
-		<img class="pic" src="http://photon.101medialablimit.netdna-cdn.com/hypebeast.com/image/2015/12/kobe-bryant-greatest-players-list-1.jpg?w=1024"
 		<br/>
 		<h3>Use Case</h3>
 		<p>A team needs to achieve a win by outscoring the other team,<br/>
