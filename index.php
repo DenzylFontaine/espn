cbc5e6f94a46f4b50031ff30b439ef79237b005b
DenzylFontaine <ddenzfong@gmail.com>
2016-01-20 20:15:03 -0700

    redoing diagram

--- index.php
+++ index.php
@@ -7,26 +7,18 @@
 	</head>
 	<body>
 		<h1>ESPN Entity Relationship</h1>
-		<img src="espn.svg"/>
+		<img src="espn-2.svg"/>
 		<img class="pic" src="http://photon.101medialablimit.netdna-cdn.com/hypebeast.com/image/2015/12/kobe-bryant-greatest-players-list-1.jpg?w=1024"/>
 		<h2>Persona</h2>
-		<P>The crowd the NBA would like to attract is men of all ages who wish to see basketball played at its highest level.<br/>
-		Also any males who is a fan of one of the NBA's many stars.<br/>
-		The NBA would also like to have a large portion of female viewers as well.<br/>
-		Mostly teenage to middle age women who have played before or simply fans of the game.<br/>
-		Much like the men the women would also have favorite players and stars who they would tune in to watch.
-		</P>
-		<br/>
+		<P>
 		<h3>Use Case</h3>
-		<p>A team needs to achieve a win by outscoring the other team,<br/>
-			by scoring points and making free throws than the other team a win can be achieved.</p>
+		<p></p>
 			<br/>
 			<h4>
 				<strong>Important Data Model: What is required to get a win?</strong>
 			</h4>
 				<ul>
-					<li>Commit as little fouls and turnovers as posible.</li>
-					<li>Score as more and basketball</li>
+					<li></li>
 				</ul>
 	</body>
 </html>
